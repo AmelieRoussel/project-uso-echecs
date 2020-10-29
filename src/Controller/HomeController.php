bdf5167e09540fc707dd6a71eebe97708a71112a
AmelieRoussel <amelie.roussel41@gmail.com>
2020-10-29 22:06:22 +0100

    html, css et controller ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -27,6 +27,36 @@ class HomeController extends AbstractController
 
     public function contact()
     {
-        return $this->twig->render('Contact/contact.html.twig');
+        $errors = [];
+
+        if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+            $message = array_map('trim', $_POST);
+            $errors = $this->validate($message);
+        }
+
+        return $this->twig->render('Contact/contact.html.twig', [
+            'errors' => $errors,
+        ]);
+    }
+
+    private function validate(array $message): array
+    {
+        if (empty(htmlentities($message['firstname']))) {
+            $errors[] = 'Le prénom ne doit pas être vide';
+        }
+        if (empty(htmlentities($message['lastname']))) {
+            $errors[] = 'Le nom ne doit pas être vide';
+        }
+        if (empty(htmlentities($message['email']))) {
+            $errors[] = 'L\'email ne doit pas être vide';
+        }
+        if (!filter_var($message['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = 'Format email invalide';
+        }
+        if (empty($message['message'])) {
+            $errors[] = 'Veuillez écrire un message';
+        }
+        return $errors ?? [];
     }
 }
+
