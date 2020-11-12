d0e36eedb1cb36eafcede0600a129e3dba0ce53a
Matthieu <bardetmatthieu@posteo.net>
2020-11-13 00:57:40 +0100

    Finished validator

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -28,14 +28,14 @@ class NewsController extends AbstractController
         $errors = [];
 
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $news = array_map('trim', $_POST);
-            $errors = $this->newsValidate($news, $_FILES['cover_image']);
+            $newsData = array_map('trim', $_POST);
+            $errors = $this->newsValidate($newsData, $_FILES['cover_image']);
             if (empty($errors)) {
                 $filename = uniqid() . '.' . pathinfo($_FILES['cover_image']['name'], PATHINFO_EXTENSION);
                 move_uploaded_file($_FILES['cover_image']['tmp_name'], 'uploads/' . $filename);
-                $news['cover_image'] = 'file.jpg';
+                $newsData['cover_image'] = 'file.jpg';
                 $newsManager = new NewsManager();
-                $newsManager->addNews($news);
+                $newsManager->addNews($newsData);
 
                 header('Location: /news/admin');
             }
@@ -48,40 +48,43 @@ class NewsController extends AbstractController
     }
 
     /**
-     * @param array $news
+     * @param array $newsData
      * @param array $files
      * @return array
      *
      *  @SuppressWarnings(PHPMD)
      */
-    private function newsValidate(array $news, array $files): array
+    private function newsValidate(array $newsData, array $files): array
     {
         $errors = [];
         $fileSize = 1000000;
         $authorizedMimes = ['image/jpeg', 'image/png', 'image/gif'];
 
-        if (empty($news['title'])) {
+        if (empty($newsData['title'])) {
             $errors[] = 'Le titre ne doit pas être vide';
         }
-        if (strlen($news['title']) > self::TITLE_LENGTH) {
+        if (strlen($newsData['title']) > self::TITLE_LENGTH) {
             $errors[] = 'Le titre doit faire moins de 255 caractères';
         }
-        if (empty($news['content'])) {
+        if (empty($newsData['content'])) {
             $errors[] = 'Le contenu de l\'article ne doit pas être vide';
         }
-        if (strlen($news['content']) > self::CONTENT_LENGTH) {
+        if (strlen($newsData['content']) > self::CONTENT_LENGTH) {
             $errors[] = 'Le contenu doit faire moins de 100000 caractères';
         }
-        if (empty($news['excerpt'])) {
+        if (empty($newsData['excerpt'])) {
             $errors[] = 'Le contenu de l\'extrait ne doit pas être vide';
         }
-        if (strlen($news['excerpt']) > self::EXCERPT_LENGTH) {
+        if (strlen($newsData['excerpt']) > self::EXCERPT_LENGTH) {
             $errors[] = 'L\'extrait doit faire moins de 1000 caractères';
         }
         if ($files['size'] > $fileSize) {
             $errors[] = 'Le fichier ne doit pas excéder ' . $fileSize / 1000000 . ' Mo';
         }
-        if (!in_array(mime_content_type($files['tmp_name']), $authorizedMimes)) {
+        if (empty($files['tmp_name'])) {
+            $errors[] = 'Le fichier ne peux pas être manquant';
+        }
+        if (!empty($files['tmp_name']) && !in_array(mime_content_type($files['tmp_name']), $authorizedMimes)) {
             $errors[] = 'Ce type de fichier n\'est pas valide';
         }
 
