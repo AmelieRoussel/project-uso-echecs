5ee202bbcc1f3e8d7e95d49c1a5d9296272de9ae
Pil <70975556+arnaudpilato@users.noreply.github.com>
2020-11-16 23:03:30 +0100

    Update AdminCompetitionManager.php

--- src/Model/AdminCompetitionManager.php
+++ src/Model/AdminCompetitionManager.php
@@ -40,15 +40,4 @@ class AdminCompetitionManager extends AbstractManager
         $statement->bindValue('picture', $item['picture']);
         $statement->execute();
     }
-
-    /**
-     * @param int $id
-     */
-    public function delete(int $id): void
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("DELETE FROM " . self::TABLE . " WHERE id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
-    }
 }
